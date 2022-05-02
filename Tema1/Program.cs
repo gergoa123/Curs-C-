571fe0839e7d388ff7014ce291597ba7606e17f5
Gergoa123 <gergo3124@gmail.com>
2022-05-02 17:44:37 +0300

    Add project files.

---
+++ Tema1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tema1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti lungimea");
+            int lungime = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Introduceti latimea");
+            int latime = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Introduceti inaltimea");
+            int inaltime = Convert.ToInt32(Console.ReadLine());
+
+            int volum = lungime * latime * inaltime;
+
+            Console.WriteLine("Volumul paralelipipedului greptunghic este " + volum);
+        }
+    }
+}
